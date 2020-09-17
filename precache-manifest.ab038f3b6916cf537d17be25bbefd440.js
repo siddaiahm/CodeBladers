b0c8ddac0bc13d021dee154a2f73ebb7c578b2fd
Siddaiah M <siddaiah.m@mountblue.tech>
2020-09-17 21:45:06 +0530

    Updates

---
+++ precache-manifest.ab038f3b6916cf537d17be25bbefd440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b23ad22051766e3d0a82dbb93a4df86",
+    "url": "/CodeBladers/index.html"
+  },
+  {
+    "revision": "e95ca9f4bbef907ea92d",
+    "url": "/CodeBladers/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbb092b1c0f9126c2a47",
+    "url": "/CodeBladers/static/js/2.c3edd6ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeBladers/static/js/2.c3edd6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95ca9f4bbef907ea92d",
+    "url": "/CodeBladers/static/js/main.975f24b9.chunk.js"
+  },
+  {
+    "revision": "f709f71161dceede505a",
+    "url": "/CodeBladers/static/js/runtime-main.31ae895f.js"
+  }
+]);
